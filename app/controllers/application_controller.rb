be1f9a1dd78c4ff9c190da621d910945214a1e1d
Kris752716 <111364330+Kris752716@users.noreply.github.com>
2022-08-17 10:12:56 +0300

    jgvjhg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   def hello
     render html: "Get out of my site NOW!!"
-    render image_tag("https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg/170px-Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg")
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg/170px-Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg" />
-  end
+   end
 end
