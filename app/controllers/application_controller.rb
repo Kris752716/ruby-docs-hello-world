5dcafbc79fa838cd95cdc8df39ab911378f29348
Kris752716 <111364330+Kris752716@users.noreply.github.com>
2022-08-17 11:50:28 +0300

    dfdsfds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   def hello
     render html: "Get out of my site NOW!!"
+    render <link href="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg/220px-Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg" />
    end
 end
